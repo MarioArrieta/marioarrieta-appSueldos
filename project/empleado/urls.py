ea9f0f9195687872ebf5717b448bd1959577005d
Mario Arrieta <marioarrieta2013@gmail.com>
2024-04-27 21:44:43 -0300

    Trabajo en la busqueda de datos - App Empleado

--- project/empleado/urls.py
+++ project/empleado/urls.py
@@ -5,6 +5,10 @@ app_name = "empleado"
 urlpatterns = [
    
     path ("inicio/", views.inicio, name = "index"),
+    path ("buscar/", views.buscar, name = "buscar"),
+    path("pagos/read/", views.pagos_read, name="pagos_read"),
+    path("vacaciones/read/", views.vacaciones_read, name="vacaciones_read"),
+    
     
     
 
--- project/empleado/views.py
+++ project/empleado/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from empleador.models import Vacaciones, Pagos
 
 # Create your views here.
-from django.http import HttpResponse
 
 def inicio (request):
     return render (request, "empleado/index.html")
+
+def buscar (request):
+    return render (request, "empleado/buscar.html")
+
+def pagos_read(request):
+    if request.method == 'GET':
+        cuil = request.GET.get('cuil')
+        if cuil:
+            pagos = Pagos.objects.filter(empleado__cuil=cuil)
+            return render(request, 'empleado/pagos_read.html', {'pagos': pagos})
+    return render(request, 'empleado/pagos_read.html', {})
+
+def vacaciones_read(request):
+    if request.method == 'GET':
+        cuil = request.GET.get('cuil')
+        if cuil:
+            vacaciones = Vacaciones.objects.filter(empleado__cuil=cuil)
+            return render(request, 'empleado/vacaciones_read.html', {'vacaciones': vacaciones})
+    return render(request, 'empleado/vacaciones_read.html', {})
+
+
